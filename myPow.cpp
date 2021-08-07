f59c716e8ba99f576f9d5233a573228156d49cec
snill901123 <82087396+snill901123@users.noreply.github.com>
2021-08-07 13:06:54 +0800

    Add files via upload

---
+++ myPow.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        double half=myPow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        if(n>0){
+            return half*half*x;
+        }
+        return half*half/x;
+}
