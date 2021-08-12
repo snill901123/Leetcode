5465380be0db1ade4a132b00526d3282e8107ba9
snill901123 <82087396+snill901123@users.noreply.github.com>
2021-08-13 01:17:44 +0800

    Update Longest Palindromic Substring.cpp

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -4,28 +4,28 @@ public:
         int len=s.length();
         int start=0,maxlength=1;
         for(int i=0;i<len-1;i++){
-            int low=i,high=i+1;  //pair condition
-            while(high<len && low>=0 && s[low]==s[high]){
-                low--;
-                high++;
+            int left=i,right=i+1;  //pair condition
+            while(right<len && left>=0 && s[left]==s[right]){
+                left--;
+                right++;
             }
-            low++;
-            high--;
-            if(high-low+1>maxlength){
-                start=low;
-                maxlength=high-low+1;
+            left++;
+            right--;
+            if(right-left+1>maxlength){
+                start=left;
+                maxlength=right-left+1;
             }
-            low=i-1;     //middle condition
-            high=i+1;
-            while(high<len && low>=0 && s[low]==s[high]){
-                low--;
-                high++;
+            left=i-1;     //middle condition
+            right=i+1;
+            while(right<len && left>=0 && s[left]==s[right]){
+                left--;
+                right++;
             }
-            low++;
-            high--;
-            if(high-low+1>maxlength){
-                start=low;
-                maxlength=high-low+1;
+            left++;
+            right--;
+            if(right-left+1>maxlength){
+                start=left;
+                maxlength=right-left+1;
             }
         }
         return s.substr(start,maxlength);
